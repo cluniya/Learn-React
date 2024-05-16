3dcbfe4bcb7da18e591acf49f6efdd7065fe52bb
cluniya <chandanluniya1986@gmail.com>
2024-05-16 09:21:12 +0530

    First Custom Component

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,11 @@
+export default function ExpenseItem () {
+    return <div>
+  
+      <h1>This is expense item!</h1>
+      <p>Food - $5</p>
+      <p>Petrol - $20</p>
+      <p>Movie - $10</p>
+      
+          </div>
+  }
+  
