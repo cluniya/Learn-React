89e03af0f212296c91a13f036a56fa00d96d553b
cluniya <chandanluniya1986@gmail.com>
2024-05-17 11:38:13 +0530

    Controlled Components - Expenses Filter

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,23 +1,39 @@
-import React from "react";
+import React,{useState} from "react";
 import "./Expenses.css"
 import ExpenseItem from "./ExpenseItem";
+import ExpensesFilter from "./ExpensesFilter";
 import Card from "./Card";
 
-function Expenses(props) {
+const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState("2023");
+
+  const changeFilterHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
+  const filteredExpenses = props.expenses.filter(
+    (expense) => expense.date.getFullYear().toString() === filteredYear
+  );
+
+
   return (
     <Card className="expenses">
-      {props.expenses.map((expense) => {
+      <ExpensesFilter
+        selected={filteredYear}
+        onChangeFilter={changeFilterHandler}
+      />
+      {filteredExpenses.map((expense) => {
         return (
           <ExpenseItem
             key={expense.id}
             title={expense.title}
             date={expense.date}
             price={expense.price}
-          ></ExpenseItem>
+          />
         );
       })}
     </Card>
   );
-}
+};
 
 export default Expenses;
